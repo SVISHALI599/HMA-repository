baed7ed3ab0ae8b2396f5f348e604e7516d97a19
Vishali <sakthivelvishali@gmail.com>
2020-01-19 11:04:33 +0530

    First Commit

--- SpringHMA/src/main/java/com/example/demo/mapper/PatientMapper.java
+++ SpringHMA/src/main/java/com/example/demo/mapper/PatientMapper.java
@@ -1,6 +1,9 @@
 package com.example.demo.mapper;
 
 import java.util.List;
+
+import javax.ws.rs.Path;
+
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
@@ -10,8 +13,10 @@ import org.apache.ibatis.annotations.Update;
 import com.example.demo.model.Patient;
 
 @Mapper
+@Path("mapper/patient")
 public interface PatientMapper {
 	@Select("SELECT user_name, user_age, user_gender, user_mobile_number, user_email_id, user_address_line1, user_address_line2, user_address_line3, patient_disease FROM t_user JOIN t_patient ON t_user.pk_user_id=t_patient.fk_user_id")
+
 	List<Patient> getAllPatient();
 
 	@Select("select * from  t_patient where pk_patient_id=#{pk_patient_id}")
@@ -28,5 +33,5 @@ public interface PatientMapper {
 	int deletePatient(int userId);
 
 	@Update("UPDATE t_user SET user_name = #{userName}, user_password=#{userPassword}, user_age=#{userAge}, user_gender=#{userGender}, user_mobile_number=#{userMobileNumber}, user_email_id=#{userEmailId}, user_address_line1=#{userAddressLine1}, user_address_line2=#{userAddressLine2}, user_address_line3 = #{userAddressLine3} WHERE pk_user_id = #{userId}")
-	int updatePatient(Patient patient);
+	int updatePatient(Patient patient,int id);
 }
